97b0173c2b0c0ff81436db3b898edba1690e619b
cooper7196 <cbrosgall@shaw.ca>
2021-11-06 15:19:18 -0700

    Added X-Drive Op-Control

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,13 @@
 #include "main.h"
 
 /**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
-
-/**
  * Runs initialization code. This occurs as soon as the program is started.
  *
  * All other competition modes are blocked by initialize; it is recommended
  * to keep execution time for this mode under a few seconds.
  */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
+void initialize()
+{
 }
 
 /**
@@ -73,20 +54,30 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
 
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
+void odometry()
+{
+}
 
-		left_mtr = left;
-		right_mtr = right;
+void opcontrol()
+{
+	pros::Controller controller(pros::E_CONTROLLER_MASTER);
+	pros::Motor front_right_mtr(10);
+	pros::Motor front_left_mtr(1);
+	pros::Motor back_right_mtr(20);
+	pros::Motor back_left_mtr(11);
+
+	while (true)
+	{
+		int joystickCh1 = controller.get_analog(ANALOG_RIGHT_X);
+		int joystickCh3 = controller.get_analog(ANALOG_LEFT_Y);
+		int joystickCh4 = controller.get_analog(ANALOG_LEFT_X);
+
+		front_right_mtr = -joystickCh3 + joystickCh1 + joystickCh4;
+		front_left_mtr = joystickCh3 + joystickCh1 + joystickCh4;
+		back_right_mtr = -joystickCh3 + joystickCh1 - joystickCh4;
+		back_left_mtr = joystickCh3 + joystickCh1 - joystickCh4;
+
 		pros::delay(20);
 	}
 }
