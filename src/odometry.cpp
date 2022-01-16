bcf077434e17694aca4f9628248204486044fd5d
cooper7196 <cbrosgall@gmail.com>
2022-01-15 16:26:22 -0800

    Fixed Odometry

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -10,7 +10,7 @@ double modulo(double a, double b)
 }
 float Sl = 6.27;               //distance from tracking center to middle of left wheel
 float Sr = 6.27;               //distance from tracking center to middle of right wheel
-float Ss = 100;                //distance from tracking center to middle of the tracking wheel
+float Ss = 5;                  //distance from tracking center to middle of the tracking wheel
 float wheelDiameter = 2.75;    //diameter of the side wheels being used for tracking
 float trackingDiameter = 2.75; //diameter of the sideways tracking wheel
 
@@ -38,8 +38,8 @@ float deltaLeft = 0;
 float deltaRight = 0;
 float deltaSide = 0;
 
-float deltaLr = 0;
-float deltaRr = 0;
+float delta_middle_pos = 0;
+float delta_perp_pos = 0;
 
 float deltaX = 0;
 float deltaY = 0;
@@ -55,7 +55,7 @@ pros::Rotation sideEncoder(19);
 
 void updatePosition()
 {
-    curLeft = -leftEncoder.get_position() / 100;
+    curLeft = leftEncoder.get_position() / 100;
     curRight = rightEncoder.get_position() / 100; //step 1
     curSide = sideEncoder.get_position() / 100;
 
@@ -63,58 +63,18 @@ void updatePosition()
     deltaRight = (curRight - lastRightPos) * (M_PI / 180) * (wheelDiameter / 2); //step 2
     deltaSide = (curSide - lastSidePos) * (M_PI / 180) * (trackingDiameter / 2);
 
-    lastLeftPos = curLeft;
+    deltaTheta = (deltaLeft - deltaRight) / (Sl + Sr); //step 5
 
-    deltaLr = (curLeft - leftAtReset) * (M_PI / 180) * (wheelDiameter / 2); //step 4
-    deltaRr = (curRight - rightAtReset) * (M_PI / 180) * (wheelDiameter / 2);
+    delta_middle_pos = (deltaLeft + deltaRight) / 2;
+    delta_perp_pos = deltaSide - Ss * deltaTheta;
 
-    thetaNew = (thetaReset + (deltaLr - deltaRr) / (Sl + Sr)); //step 5
-    // if (thetaNew >= M_PI) {
-    //   thetaNew-=2*M_PI;
-    // }
-    // else if (thetaNew <= -M_PI) {
-    //   thetaNew+=2*M_PI;
-    // }
+    deltaX = delta_middle_pos * cos(angle) - delta_perp_pos * sin(angle);
+    deltaY = delta_middle_pos * sin(angle) + delta_perp_pos * cos(angle);
 
-    deltaTheta = thetaNew - angle; //step 6
-
-    // deltaSide = deltaSide - Ss * deltaTheta;
-
-    if (deltaTheta == 0)
-    {
-        deltaX = deltaSide; //step 7
-        deltaY = deltaRight;
-    }
-    else
-    {
-        deltaX = (2 * sin(deltaTheta / 2)) * (deltaSide / deltaTheta + Ss); //step 8
-        deltaY = (2 * sin(deltaTheta / 2)) * (deltaRight / deltaTheta + Sr);
-    }
-
-    // thetaM = angle + deltaTheta / 2; //step 9
-
-    // theta = atan2(deltaY, deltaX);
-    // radius = sqrt(deltaX * deltaX + deltaY * deltaY);
-    // theta = theta - thetaM; //step 10
-    // deltaX = radius * cos(theta);
-    // deltaY = radius * sin(theta);
-
-    // xPos = xPos - deltaX; //step 11
-    // yPos = yPos + deltaY;
-
-    float p = (thetaNew / 2) + angle; // The global ending angle of the robot
-
-    float cosP = cos(p);
-    float sinP = sin(p);
-
-    yPos += deltaY * cosP;
-    xPos += deltaY * sinP;
-
-    yPos += deltaX * -sinP; // -sin(x) = sin(-x)
-    xPos += deltaX * cosP;  // cos(x) = cos(-x)
-
-    angle = thetaNew;
-    printf("x: %f, y: %f, theta: %f, deltaX: %f, deltaY: %f    time:%i\n", xPos, yPos, angle, deltaX, deltaY, ((uint32_t)pros::millis() - (uint32_t)lastTime));
+    xPos += deltaX;
+    yPos += deltaY;
+    angle += deltaTheta;
+    printf("x: %f, y: %f, theta: %f, deltaX: %f, deltaY: %f    time:%i\n", xPos, yPos, angle * 180 / M_PI, deltaX, deltaY, ((uint32_t)pros::millis() - (uint32_t)lastTime));
     lastLeftPos = curLeft;
     lastRightPos = curRight;
     lastSidePos = curSide;
@@ -136,7 +96,7 @@ void odometry(void *odometryArgs)
     // pros::Motor front_left_mtr(1);
     // pros::Motor back_right_mtr(20);
     // pros::Motor back_left_mtr(11);
-
+    /*
     okapi::Motor frontLeftMotor(1, false, okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
     okapi::Motor frontRightMotor(10, true, okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
     okapi::Motor backRightMotor(20, true, okapi::AbstractMotor::gearset::green, okapi::AbstractMotor::encoderUnits::degrees);
@@ -153,11 +113,13 @@ void odometry(void *odometryArgs)
                                                                 .withDimensions(okapi::AbstractMotor::gearset::green, {{4_in, 11.5_in}, okapi::imev5GreenTPR}) //gearset, diameter, track, ticks
                                                                 .withOdometry({{2.75_in, 14_in, 4_in, 2.75_in}, 360})
                                                                 .buildOdometry(); // build an odometry chassis
+    */
+    leftEncoder.set_reversed(true);
 
     while (true)
     {
-        // updatePosition();
-        printf("x: %f\n", chassis->getState().x.convert(okapi::inch));
+        updatePosition();
+        // printf("x: %f\n", chassis->getState().x.convert(okapi::inch));
         pros::delay(10);
 
         //https://gm0.org/en/latest/docs/software/odometry.html How to odometry
