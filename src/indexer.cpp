e709528a53f9dba99cbd0127249de8c845d4a9f6
Cooper7196 <cbrosgall@shaw.ca>
2023-01-28 19:51:36 -0800

    Fixed Indexer

--- src/indexer.cpp
+++ src/indexer.cpp
@@ -5,7 +5,7 @@ Indexer::Indexer(pros::Motor *motor)
     this->motor = motor;
     this->motor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     this->motor->set_reversed(true);
-
+    this->motor->set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
     this->task = new pros::Task(this->taskFn, (void *)this);
 }
 
@@ -15,17 +15,17 @@ void Indexer::run()
     {
         if (this->isShooting)
         {
-            if (this->motor->get_position() - 100 > -20)
+            if (this->motor->get_position() - 360 > -20)
             {
                 this->hasShot = true;
-                this->motor->move_absolute(0, 200);
-            }
-            if (this->hasShot && this->motor->get_position() < 5)
-            {
                 this->isShooting = false;
-                this->hasShot = false;
-                this->motor->move(-20);
             }
+            // if (this->hasShot && this->motor->get_position() < 5)
+            // {
+            //     this->isShooting = false;
+            //     this->hasShot = false;
+            //     // this->motor->move(-20);
+            // }
         }
         pros::delay(20);
     }
@@ -37,7 +37,7 @@ void Indexer::shoot()
     {
         this->isShooting = true;
         this->motor->tare_position();
-        this->motor->move_absolute(100, 200);
+        this->motor->move_absolute(360, 200);
     }
 }
 
