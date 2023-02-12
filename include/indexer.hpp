186810f77428e44a088053198003e7e54398e9e5
Cooper7196 <cbrosgall@shaw.ca>
2023-02-11 16:59:46 -0800

    Added potentiometer to indexer

--- include/indexer.hpp
+++ include/indexer.hpp
@@ -4,14 +4,16 @@
 class Indexer
 {
 public:
-    Indexer(pros::Motor *motor);
+    Indexer(pros::Motor *motor, pros::ADIPotentiometer *potentiometer);
     void run();
-    void shoot();
+    void shoot(bool is_blocking = false);
     static void taskFn(void *param);
 
 private:
     pros::Motor *motor;
+    pros::ADIPotentiometer *potentiometer;
     pros::Task *task;
+    PID *indexerPID;
     bool isShooting = false;
     bool hasShot = false;
 };
--- src/indexer.cpp
+++ src/indexer.cpp
@@ -1,11 +1,14 @@
 #include "main.h"
 
-Indexer::Indexer(pros::Motor *motor)
+Indexer::Indexer(pros::Motor *motor, pros::ADIPotentiometer *potentiometer)
 {
     this->motor = motor;
     this->motor->set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
     this->motor->set_reversed(true);
     this->motor->set_encoder_units(pros::E_MOTOR_ENCODER_DEGREES);
+    this->potentiometer = potentiometer;
+    this->indexerPID = new PID(0.07, 0, 0.01, 0);
+    indexerPID->set_target(870);
     this->task = new pros::Task(this->taskFn, (void *)this);
 }
 
@@ -19,25 +22,31 @@ void Indexer::run()
             {
                 this->hasShot = true;
                 this->isShooting = false;
+                indexerPID->set_target(870);
             }
-            // if (this->hasShot && this->motor->get_position() < 5)
-            // {
-            //     this->isShooting = false;
-            //     this->hasShot = false;
-            //     // this->motor->move(-20);
-            // }
+        }
+        else
+        {
+            this->motor->move(-indexerPID->calculate(this->potentiometer->get_value()));
         }
         pros::delay(20);
     }
 }
 
-void Indexer::shoot()
+void Indexer::shoot(bool is_blocking)
 {
     if (!this->isShooting)
     {
         this->isShooting = true;
         this->motor->tare_position();
         this->motor->move_absolute(360, 200);
+    }
+    if (is_blocking)
+    {
+        while (this->isShooting)
+        {
+            pros::delay(20);
+        }
     }
 }
 
