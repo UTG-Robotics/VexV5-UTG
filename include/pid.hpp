7790c02f6335ec54ef4543b0754dd38e00f64bfc
Cooper7196 <cbrosgall@shaw.ca>
2023-02-11 17:03:26 -0800

    PID exit conditions

--- include/pid.hpp
+++ include/pid.hpp
@@ -9,11 +9,16 @@ private:
     double lastError;
     double derivative;
     double integral;
+    double timeOutTimer = 0;
+    double stopTimer = 0;
 
 public:
     double error;
+    double target;
     PID(double _Ki, double _Kp, double _Kd, double _KiStart = 999999);
+    void set_target(double target);
     void set_constants(double p, double i, double d, double iStart);
     double calculate(double error);
+    bool exit(double tolerance, double stop_time, double timeout_time);
     void clear();
 };
--- src/pid.cpp
+++ src/pid.cpp
@@ -12,13 +12,23 @@ void PID::clear()
 {
     this->integral = 0;
     this->lastError = 0;
+    this->error = 0;
+    this->stopTimer = 0;
+    this->timeOutTimer = 0;
+    return;
+}
+void PID::set_target(double target)
+{
+    this->clear();
+    this->target = target;
+    this->error = 999999;
     return;
 }
 
-double PID::calculate(double error)
+double PID::calculate(double current)
 {
     this->lastError = this->error;
-    this->error = error;
+    this->error = this->target - current;
 
     if (this->Ki != 0)
     {
@@ -35,6 +45,20 @@ double PID::calculate(double error)
 
     derivative = this->error - this->lastError;
     return this->Kp * this->error + this->Ki * integral + this->Kd * derivative;
+}
+
+bool PID::exit(double tolerance, double stop_time, double timeout_time)
+{
+    if (abs(this->error) < tolerance)
+    {
+        this->stopTimer += 0.02;
+    }
+    else
+    {
+        this->stopTimer = 0;
+    }
+    this->timeOutTimer += 0.2;
+    return this->stopTimer > stop_time || this->timeOutTimer > timeout_time;
 }
 
 // void PID::setConstants(double p, double i, double d, double iStart)
